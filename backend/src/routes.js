a8070735bc8201d060e5d7bed154a0a96fd7b9c6
João Victor <joaovictorramalho7@gmail.com>
2021-03-11 19:26:49 -0300

    Routes to show and index

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,14 @@
 const { Router } = require('express');
 const NaverController = require('./controllers/naversController');
+const ProjectsController = require('./controllers/projectsController');
 
 const routes = Router();
 
 routes.get('/navers', NaverController.index);
 routes.get('/navers/show', NaverController.show); 
 routes.post('/navers', NaverController.store);
+
+routes.get('/projects', ProjectsController.index);
+routes.get('/projects/show', ProjectsController.show);
 
 module.exports = routes;
