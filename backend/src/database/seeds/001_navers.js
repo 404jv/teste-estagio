6d1a99f2fdf980b69ab275a39871f4db57cd916c
João Victor <joaovictorramalho7@gmail.com>
2021-03-12 21:06:16 -0300

    Update seed

--- backend/src/database/seeds/001_navers.js
+++ backend/src/database/seeds/001_navers.js
@@ -15,6 +15,13 @@ exports.seed = function(knex) {
           birthdate: new Date(1998-06-12), 
           admission_date: new Date(2018-06-12), 
           job_role: 'Desenvolvedor'
+        },
+
+        {
+          name: 'João Victor', 
+          birthdate: new Date(2004-01-15), 
+          admission_date: new Date(2021-03-14), 
+          job_role: 'Desenvolvedor'
         }
       ]);
     });
